4a2e181f5f785e5ad10f62bc4cebae07b8f24912
Taylor Andrews <taylorandrews@taylor-mbp.local>
2017-08-24 16:39:11 -0600

    categorization function work

--- read_files.py
+++ read_files.py
@@ -21,13 +21,13 @@ def read_in(file1, file2):
                              'date',
                              'del',
                              'desc',
-                             'amt']
+                             'amount']
                              )
     wf = pd.read_csv(file2,
                      header=None,
                      names=
                         ['date',
-                         'amt',
+                         'amount',
                          'del',
                          'del1',
                          'desc'])
@@ -50,7 +50,7 @@ def organize(dfs):
         - dataframe with all transaction data, all types, categories unknown [dataframe]
     """
 
-    desired_cols = ['date', 'amt', 'desc', 'category', 'type']
+    desired_cols = ['merchant', 'date', 'amount', 'category', 'subcategory', 'desc']
     for df in dfs:
         for col in df.columns:
             if col[0:3] == 'del':
@@ -61,8 +61,9 @@ def organize(dfs):
         df['trans_id'] = df['trans_id'] + df.index
         df['trans_id'] = df.trans_id.map(str) + df.name
         df.set_index('trans_id', inplace=True)
-        df['type'] = 'unknown'
+        df['merchant'] = 'unknown'
         df['category'] = 'unknown'
+        df['subcategory'] = 'unknown'
         df = df[desired_cols]
     return pd.concat(dfs)
 
@@ -80,34 +81,43 @@ def categorize(trans, categories_filepath):
     """
 
     columns = ['raw_desc', 'desc', 'cat', 'type']
+    avail_subcat = ['debt_payment', 'savings', 'rent', 'car', 'clothes', 'exercise', 'gifts', 'groceries', 'life', 'recreation', 'restaurants', 'travel', 'other']
 
     cats_raw = pd.read_csv(categories_filepath)
-    cats_raw['list'] = list(zip(cats_raw.desc, cats_raw.cat, cats_raw.type))
+    cats_raw['list'] = list(zip(cats_raw.merchant, cats_raw.subcategory, cats_raw.category))
     category_map = dict(zip(list(cats_raw['raw_desc']), list(cats_raw['list'])))
+    print ":", category_map
 
     for idx, row in trans.iterrows():
         raw_desc = trans.loc[idx]['desc']
         if raw_desc[0:10] in category_map.keys():
-            trans.loc[idx,'desc'] = category_map[raw_desc[0:10]][0]
-            trans.loc[idx,'category'] = category_map[raw_desc[0:10]][1]
-            trans.loc[idx,'type'] = category_map[raw_desc[0:10]][2]
+            trans.loc[idx,'merchant'] = category_map[raw_desc[0:10]][0]
+            trans.loc[idx,'subcategory'] = category_map[raw_desc[0:10]][1]
+            trans.loc[idx,'category'] = category_map[raw_desc[0:10]][2]
         else:
-            print 'new trans desc: {} (${})'.format(raw_desc, trans.loc[idx]['amt'])
-            new_desc = raw_input("what should the descrpition be? ('s'kip, 'l'ist) > ")
+            print 'new trans desc: {} (${})'.format(raw_desc, trans.loc[idx]['amount'])
+            new_desc = raw_input("merchant? ('s'kip, 'l'ist) > ")
             if new_desc == 's':
                 pass
             else:
                 if new_desc == 'l':
                     for item in cats_raw.desc:
                         print item
-                    new_desc = raw_input("what should the descrpition be? > ")
-                new_category = raw_input('what should the category be? > ')
-                if trans.loc[idx]['amt'] < 0:
-                    new_type = 'payment'
+                    new_merchant = raw_input("merchant? > ")
+                new_subcategory = raw_input("subcategory? 'c'hoices > ")
+                if new_subcategory == 'c':
+                    print avail_subcat
+                    new_subcategory = raw_input("subcategory? > ")
+                if trans.loc[idx]['amount'] < 0:
+                    new_category = 'expense'
                 else:
-                    new_type = 'deposit'
-                category_map[raw_desc[0:10]] = [new_desc, new_category, new_type]
+                    new_type = 'revenue'
+                category_map[raw_desc[0:10]] = [new_merchant, new_subcategory, new_category]
                 print 'thanks. new merchant added.'
+
+############################################# ended here
+######### validate this function
+######### then break up the df with small dataset
 
     pd.DataFrame(
         np.hstack(
@@ -124,7 +134,7 @@ if __name__ == '__main__':
     year = '2017'
     chase_raw = '../data/{}/{}/chase_{}.csv'.format(year, month, month)
     wf_raw = '../data/{}/{}/wf_{}.csv'.format(year, month, month)
-    cats = '../data/categories.csv'
+    cats = '../data/categories_new.csv'
     dfs = read_in(chase_raw, wf_raw)
     trans = organize(dfs)
     # print trans
